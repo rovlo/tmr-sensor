a13e24eb323a08df8aabea6b01262a834ae95459
rovlo <45632997+rovlo@users.noreply.github.com>
2018-12-06 14:06:51 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <msp430.h> 
 
-#define TimerA_constant 12000
+#define TimerA_constant 6000
 
-unsigned int avg;
-signed int startValue, currentValue;
-unsigned char i, j, activityFlag, signalFalling, signalRising;
+signed int avg, startValue, currentValue;
+unsigned char i, j, activityFlag, signalFalling, signalRising, passThroughZero;
 
-volatile signed int savedSamples[20];
+volatile signed int savedSamples[30];
 
 void Clock_config(void){
     BCSCTL2 = SELM_0 | DIVM_0 | DIVS_0; //SELM_0 - Master Clock DCOCLK
@@ -66,8 +65,9 @@ int main(void)
     startValue = startValue / 5; // average out conversion results
 
     Timer_config();
+
     __bis_SR_register(LPM3_bits + GIE);
-    while(1);
+
     return 0;
 }
 
@@ -80,21 +80,20 @@ __interrupt void ADC10(void){
 __interrupt void Timer_A(void){
     TACTL &= ~MC_1; // stop timer
 
-    P1OUT ^= BIT0;
+    P1OUT |= BIT0;
 
     ADC10CTL0 |= ADC10SC;
     while((ADC10BUSY & ADC10CTL0));
     currentValue = ADC10MEM;
 
     if(currentValue - startValue > 10 || currentValue - startValue < -10){
-        P1OUT ^= BIT6;
         __bic_SR_register(LPM3_bits); //turn on CPU
         //activity detected
         //reconfigure ADC10 for slow sampling
 
         ADC10CTL0 &= ~ENC;
-        BCSCTL1 |= DIVA_1; //slow down ACLK by 2 -> 6000 Hz
-        ADC10CTL0 |= ADC10SHT_3; // slow down sampling rate to 6000/(13 ADC10CLK + 64 SHT) = 77 Hz
+        BCSCTL1 |= DIVA_2; //slow down ACLK by 2 -> 6000 Hz
+        ADC10CTL0 |= ADC10SHT_3; // slow down sampling rate to 3000/(13 ADC10CLK + 64 SHT) = 35 Hz
         //ADC10DTC0 |= ADC10CT;
         //ADC10DTC1 = 20;
         //ADC10SA = (unsigned int) &savedSamples[0]
@@ -103,37 +102,57 @@ __interrupt void Timer_A(void){
 
         signalFalling = 0;
         signalRising = 0;
+        passThroughZero = 0;
         // we measure 4 samples, average them out and store them as a single sample because of lacking memory
-        for(i = 0; i < 20; i++){
+        for(i = 0; i < 30; i++){
 
             avg = 0;
-            for(j = 0; j < 6; j++){
+            for(j = 0; j < 4; j++){
                 ADC10CTL0 |= ADC10SC;
                 while((ADC10BUSY & ADC10CTL0));
                 avg += ADC10MEM;
             }
-            avg = avg/6;
+            avg = avg/4;
             avg -= startValue;
             savedSamples[i] = avg;
 
-            if(i > 0 && avg < savedSamples[i-1]){
+            /*if(i > 0 && avg < savedSamples[i-1]){
                 signalFalling++; // counts number of times signal was falling
             } else if (i > 0 && avg > savedSamples[i-1]) {
                 signalRising++; // counts number of times signal was rising
+            }*/
+
+            if(i > 0){
+                if(avg < savedSamples[i-1]){
+                    signalFalling++;
+                } else if(avg > savedSamples[i-1]){
+                    signalRising++;
+                }
+
+                if(avg >= 0 && savedSamples[i-1] < 0 || avg <= 0 && savedSamples[i-1] > 0){
+                    passThroughZero++;
+                }
             }
+
         }
+
+        if(passThroughZero >= 2 && signalRising >= 15 && signalFalling <= 15){
+            P1OUT |= BIT6;
+            //activityFlag = 1;
+        }
+
         ADC10CTL0 &= ~ENC;
         ADC10CTL0 &= ~ADC10SHT_3;
-        BCSCTL1 &= ~DIVA_1;
+        BCSCTL1 &= ~DIVA_2;
         ADC10CTL0 |= ENC;
 
-        __bis_SR_register_on_exit(LPM3_bits); //
+        __bis_SR_register_on_exit(LPM3_bits);
 
     } else {
 
     }
 
-    P1OUT ^= BIT0;
+    P1OUT &= ~BIT0;
     TACTL |= MC_1;
 
 }
